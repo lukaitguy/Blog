2d88b092b3336a2e3f87ac8533e48c214659bf1c
lukaitguy <ldjukic00@gmail.com>
2025-03-23 23:14:30 +0100

    Added new user managment functionality for admins. Adding and deleting new users and assigning roles.

---
+++ Blog.Web/Controllers/AdminUsersController.cs
@@ -0,0 +1,88 @@
+﻿using Blog.Web.Models.ViewModels;
+using Blog.Web.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Blog.Web.Controllers
+{
+    public class AdminUsersController : Controller
+    {
+        private readonly IUserRepository userRepository;
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AdminUsersController(IUserRepository userRepository, UserManager<IdentityUser> userManager)
+        {
+            this.userRepository = userRepository;
+            this.userManager = userManager;
+        }
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var users = await userRepository.GetAll();
+            var usersViewModel = new UserViewModel();
+            usersViewModel.Users = new List<User>();
+
+            foreach (var user in users)
+            {
+                usersViewModel.Users.Add(new Models.ViewModels.User
+                {
+                    Id = Guid.Parse(user.Id),
+                    Username = user.UserName,
+                    EmailAddress = user.Email
+                });
+            }
+
+            return View(usersViewModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> List(UserViewModel request)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = request.Username,
+                Email = request.Email,
+            };
+
+            var identityResult = await userManager.CreateAsync(identityUser, request.Password);
+
+            if(identityResult is not null)
+            {
+                if (identityResult.Succeeded)
+                {
+                    // Assign roles to user
+                    var roles = new List<string>
+                    {
+                        "User"
+                    };
+                    if(request.AdminRoleCheckBox)
+                    {
+                        roles.Add("Admin");
+                    }
+                    identityResult = await userManager.AddToRolesAsync(identityUser, roles);
+
+                    if(identityResult is not null && identityResult.Succeeded)
+                    {
+                        return RedirectToAction("List", "AdminUsers");
+                    }
+                }
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var user = await userManager.FindByIdAsync(id.ToString());
+
+            if(user is not null)
+            {
+                var identityResult = await userManager.DeleteAsync(user);
+                if(identityResult is not null && identityResult.Succeeded)
+                {
+                    return RedirectToAction("List", "AdminUsers");
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ Blog.Web/Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using Blog.Web.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Web.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AuthDbContext authDbContext;
+
+        public UserRepository(AuthDbContext authDbContext)
+        {
+            this.authDbContext = authDbContext;
+        }
+        public async Task<IEnumerable<IdentityUser>> GetAll()
+        {
+            var users = await authDbContext.Users.ToListAsync();
+
+            var superAdmin = await authDbContext.Users.FirstOrDefaultAsync(u => u.Email == "superadmin@blog.com");
+
+            if(superAdmin is not null)
+            {
+                users.Remove(superAdmin);
+            }
+            return users;
+        }
+    }
+}
